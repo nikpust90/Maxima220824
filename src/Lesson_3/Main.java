1dd5cdeacc390d525620f45bf5473d01c0d6ef13
nikpust90 <nikolai.first21@yandex.ru>
2024-08-26 21:45:50 +0300

    9

--- src/Lesson_3/Main.java
+++ src/Lesson_3/Main.java
@@ -9,20 +9,20 @@ public class Main {
         //- выводил на экран строку "Число меньше 5", если число, которое мы ей подадим меньше 5,
         //- выводил строку "Число больше 5", если число, которое мы ей подадим больше 5,
         //- выводил строку "Число равно 5", если число, которое мы ей подадим равен 5.
-//        Scanner scanner = new Scanner(System.in);
-//        int a = scanner.nextInt();
-//
-//        if (a < 5) {
-//            System.out.println("Число меньше 5");
-//        }
-//
-//        if (a > 5) {
-//            System.out.println("Число больше 5");
-//        }
-//
-//        if (a == 5) {
-//            System.out.println("Число равно 5");
-//        }
+        Scanner scanner = new Scanner(System.in);
+        int a = scanner.nextInt();
+
+        if (a < 5) {
+            System.out.println("Число меньше 5");
+        }
+
+        if (a > 5) {
+            System.out.println("Число больше 5");
+        }
+
+        if (a == 5) {
+            System.out.println("Число равно 5");
+        }
 
 
         //2 Напиши программу. которая будет проверять попало ли целое число, которое мы ей подадим
@@ -35,13 +35,13 @@ public class Main {
         //Пример для числа 60:
         //Число 60 содержится в интервале.
 
-//        Scanner scanner1 = new Scanner(System.in);
-//        int entered_number = scanner1.nextByte();
-//        if (entered_number >= 50 && entered_number <= 100){
-//            System.out.println("Число " + entered_number + " содержится В интервале.");
-//        } else {
-//            System.out.println("Число " + entered_number + " НЕ содержится в интервале.");
-//        }
+        Scanner scanner1 = new Scanner(System.in);
+        int entered_number = scanner1.nextByte();
+        if (entered_number >= 50 && entered_number <= 100){
+            System.out.println("Число " + entered_number + " содержится В интервале.");
+        } else {
+            System.out.println("Число " + entered_number + " НЕ содержится в интервале.");
+        }
 
 
         //3 Напиши программу, которая по номеру месяца должна определить время года (зима, весна, лето, осень) и вывести на экран.
@@ -51,20 +51,20 @@ public class Main {
         //Пример для номера месяца 5:
         //весна
 
-//        Scanner scanner2 = new Scanner(System.in);
-//        int timeOfYear = scanner2.nextByte();
-//        if (timeOfYear < 3 || timeOfYear == 12) {
-//            System.out.println("Время года зима");
-//        }
-//        else if (timeOfYear >= 3 && timeOfYear <= 5) {
-//            System.out.println("Время года весна");
-//        }
-//        else if (timeOfYear >= 6 && timeOfYear <= 8) {
-//            System.out.println("Время года лето");
-//        }
-//        else if (timeOfYear >= 9 && timeOfYear <= 11) {
-//            System.out.println("Время года осень");
-//        }
+        Scanner scanner2 = new Scanner(System.in);
+        int timeOfYear = scanner2.nextByte();
+        if (timeOfYear < 3 || timeOfYear == 12) {
+            System.out.println("Время года зима");
+        }
+        else if (timeOfYear >= 3 && timeOfYear <= 5) {
+            System.out.println("Время года весна");
+        }
+        else if (timeOfYear >= 6 && timeOfYear <= 8) {
+            System.out.println("Время года лето");
+        }
+        else if (timeOfYear >= 9 && timeOfYear <= 11) {
+            System.out.println("Время года осень");
+        }
 
         //4
         //Написать программу, которая будет проверять число, которое мы подадим ей.
@@ -73,16 +73,16 @@ public class Main {
         //Если введенное число равно нулю, необходимо вывести ноль.
         //Вывести результат в консоль.
 
-//        Scanner scanner2 = new Scanner(System.in);
-//        int positiveOrNegativeNumber = scanner2.nextByte();
-//        if (positiveOrNegativeNumber > 0){
-//            positiveOrNegativeNumber = positiveOrNegativeNumber * 2;
-//        } else if (positiveOrNegativeNumber < 0) {
-//            positiveOrNegativeNumber = positiveOrNegativeNumber + 1;
-//        } else if (positiveOrNegativeNumber == 0) {
-//            positiveOrNegativeNumber = 0;
-//        }
-//        System.out.println(positiveOrNegativeNumber);
+        Scanner scanner2 = new Scanner(System.in);
+        int positiveOrNegativeNumber = scanner2.nextByte();
+        if (positiveOrNegativeNumber > 0){
+            positiveOrNegativeNumber = positiveOrNegativeNumber * 2;
+        } else if (positiveOrNegativeNumber < 0) {
+            positiveOrNegativeNumber = positiveOrNegativeNumber + 1;
+        } else if (positiveOrNegativeNumber == 0) {
+            positiveOrNegativeNumber = 0;
+        }
+        System.out.println(positiveOrNegativeNumber);
 
 
 
@@ -105,14 +105,33 @@ public class Main {
         //Годы 1600 и 2000 - високосные, так как они кратны 100 и кратны 400.
         //Годы 2100, 2200 и 2300 - не високосные.
 
-//        Scanner scanner2 = new Scanner(System.in);
-//        double entered_number = scanner2.nextByte();
-//        double divisionBy4 = entered_number / 4;
-//        if (divisionBy4 % 1 == 0){
-//            System.out.println("количество дней в этом году: 366");
-//        } else {
-//
-//        }
+        Scanner scanner2 = new Scanner(System.in);
+        double year = scanner2.nextInt();
+
+        boolean is_leap_year = false;
+
+
+        if (year % 4 == 0) {
+            is_leap_year = true;
+
+
+            if (year % 100 == 0) {
+
+                if (year % 400 == 0)
+                    is_leap_year = true;
+                else
+                    is_leap_year = false;
+            }
+        }
+        else {
+            is_leap_year = false;
+        }
+
+        if (!is_leap_year)
+            System.out.println(year + " Не высокостный год");
+        else
+            System.out.println(year + " Высокостный год");
+
 
 
         //7
@@ -129,16 +148,16 @@ public class Main {
         //Если хотя бы в одном случае сторона окажется больше либо равна сумме двух других,
         // то треугольника с такими сторонами не существует.
 
-//        int a = 5;
-//        int b = 5;
-//        int c = 10;
-//
-//        if (a + b > c && a + c > b && b + c > a){
-//
-//            System.out.println("Треугольник существует");
-//        } else {
-//            System.out.println("Треугольник НЕ существует");
-//        }
+        int a = 5;
+        int b = 5;
+        int c = 10;
+
+        if (a + b > c && a + c > b && b + c > a){
+
+            System.out.println("Треугольник существует");
+        } else {
+            System.out.println("Треугольник НЕ существует");
+        }
 
         //8
         // Работа светофора для пешеходов запрограммирована следующим образом:
@@ -169,26 +188,26 @@ public class Main {
         //Пример для числа 5:
         //зелёный
 
-//        int interval_green = 3;
-//        int interval_red = 4;
-//        int interval_yellow = 5;
-//
-//        Scanner scanner2 = new Scanner(System.in);
-//        double entered_number = scanner2.nextByte();
-//        //double entered_number = 48;
-//        double a1 = entered_number / interval_yellow;//1
-//        if (a1 % 1 == 0 || entered_number <= interval_green){
-//            System.out.println("Горит Желтый");
-//        } else {
-//            double g1 = a1 - (a1 % 1);
-//            double c1 = g1 * interval_yellow;
-//            double remainder_of_the_number = entered_number - c1;
-//            if (remainder_of_the_number <= interval_green){
-//                System.out.println("Горит Зеленый");
-//            } else {
-//                System.out.println("Горит Красный");
-//            }
-//        }
+        int interval_green = 3;
+        int interval_red = 4;
+        int interval_yellow = 5;
+
+        Scanner scanner2 = new Scanner(System.in);
+        double entered_number = scanner2.nextByte();
+        //double entered_number = 48;
+        double a1 = entered_number / interval_yellow;//1
+        if (a1 % 1 == 0 || entered_number <= interval_green){
+            System.out.println("Горит Желтый");
+        } else {
+            double g1 = a1 - (a1 % 1);
+            double c1 = g1 * interval_yellow;
+            double remainder_of_the_number = entered_number - c1;
+            if (remainder_of_the_number <= interval_green){
+                System.out.println("Горит Зеленый");
+            } else {
+                System.out.println("Горит Красный");
+            }
+        }
 
     }
 }
---
+++ src/Lesson_4/Main.java
@@ -0,0 +1,14 @@
+package Lesson_4;
+
+import javax.sound.sampled.SourceDataLine;
+
+public class Main {
+    public static void main(String[] args) {
+
+
+        while (true){
+
+        }
+
+    }
+}
